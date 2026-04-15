d0918b128966e9531f7bd3bc10f190888ca91404
dcostaprakash <starfinhub@gmail.com>
2026-04-15 14:46:37 +0100

    GLASGOW | 26-ITP-Jan | Prakash Dcosta | Sprint 2 | Book Library

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,20 +6,3 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
-
-function takeOutOrder(order) {
-  let totalSum = 0;
-
-  console.log("OTY       ITEM             TOTAL");
-  order.forEach(({ quantity, itemName, unitPricePence }) => {
-    const itemTotal = (quantity * unitPricePence) / 100;
-    totalSum += itemTotal;
-
-    console.log(
-      `${quantity}       ${itemName.padEnd(18)} ${itemTotal.toFixed(2)}`
-    );
-  });
-
-  console.log(`\nTotal: ${totalSum.toFixed(2)}`);
-}
-takeOutOrder(order);
